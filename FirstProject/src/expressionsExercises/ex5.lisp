be77e5d5cc9bc1525718cced70fe2db76e5fc94a
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-12-21 17:24:18 +0100

    [add] ex5 in expressionsExercises - FirstProject

---
+++ FirstProject/src/expressionsExercises/ex5.lisp
@@ -0,0 +1,32 @@
+(defun main ()
+  "Main function of the program."
+  (let ((seconds (get-seconds)))
+    (format t
+            "~a:~a:~a~&"
+            (seconds-to-hours seconds)
+            (seconds-to-minutes seconds t)
+            (remainder-seconds seconds))))
+
+(defun get-seconds ()
+  "Ask the user to enter seconds and return its value."
+  (print "Enter the seconds you want to convert: ")
+  (read))
+
+(defun seconds-to-hours (seconds)
+  "Return the number of hours there are in n seconds"
+  (floor seconds 3600))
+
+(defun seconds-to-minutes (seconds &optional remainder-from-hour-p)
+  "Return the number of minutes there are in n seconds or if
+REMAINDER-FROM-HOUR-P is non-nil the remainder minutes from calculating the
+hours."
+  (if remainder-from-hour-p
+      (floor (remainder-seconds seconds t) 60)
+      (floor seconds 60)))
+
+(defun remainder-seconds (seconds &optional remainder-from-hour-p)
+  "Return the remainder of seconds from calculating the minutes or if
+REMAINDER-FROM-HOUR-P is non-nil from calculating the hours."
+  (if remainder-from-hour-p
+      (mod seconds 3600)
+      (mod (remainder-seconds seconds t) 60)))
