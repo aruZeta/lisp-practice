d61fd4a1ed76478533fae11969e2c3187027a85c
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-12-23 11:24:20 +0100

    [add] ex9 in expressionsExercises - FirstProject

---
+++ FirstProject/src/expressionsExercises/ex9.lisp
@@ -0,0 +1,23 @@
+;;;; Make a program that asks the user for a number and shows it reversed
+
+(defun main ()
+  "Main function of the program."
+  (let ((number (get-number)))
+    (format t
+            "~a reversed: ~a~&"
+            number
+            (reverse-n number))))
+
+(defun get-number ()
+  "Ask the user to enter a number and return its value."
+  (print "Enter a number: ")
+  (read))
+
+(defun reverse-n (n)
+  "Return the reverse of the number passed."
+  (let ((reversed-n 0))
+    (loop while (> n 0) do
+      (setq reversed-n (+ (* reversed-n 10)
+                          (mod n 10))
+            n (floor n 10)))
+    reversed-n))
