2da9415216031bb6587736acdc4ca3050e93686c
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-12-25 23:41:30 +0100

    [add, refactor, test] Testing typechecking for variables
    Common lisp is really fun, planning to add this to the functions too!
    
    Also I tried to setup asdf to make compilation, packaging and stuff
    easier, like keeping types in a `type.lip` file, functions for getting
    input in another, etc. But still no luck, have to research it a bit
    more.

--- SelectionStructures/src/simpleAndDouble/marital-status.lisp
+++ SelectionStructures/src/simpleAndDouble/marital-status.lisp
@@ -1,16 +1,42 @@
-;;;; Make a program that asks the user the character representing his marital
-;;;; status and shows the complete name
+;;;; Make a program that asks the user the character (alias) representing his
+;;;; marital status and shows the complete name
 
-(defvar *marital-status-list*
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun list-of-alist-char-list-to-string-p (object)
+    "Return T if OBJECT is a LIST containing ALIST of CHARACTER LIST to STRING.
+Otherwise, return NIL."
+    (when (listp object)
+      (every #'alist-char-list-to-string-p object)))
+
+  (defun character-list-p (object)
+    "Return T if OBJECT is a LIST containing CHARACTER.
+Otherwise, return NIL."
+    (when (listp object)
+      (every #'characterp object)))
+
+  (defun alist-char-list-to-string-p (object)
+    "Return T if OBJECT is an ALIST containing CHARACTER LIST to STRING.
+Otherwise, return NIL."
+    (when (consp object)
+      (and (character-list-p (car object))
+           (stringp (cdr object))))))
+
+(deftype list-of-alist-char-list-to-string ()
+  "Type which must be a LIST containing ALIST of CHARACTER LIST to STRING."
+  `(satisfies list-of-alist-char-list-to-string-p))
+
+(declaim (type (list-of-alist-char-list-to-string)
+               *alias-to-marital-status-list*))
+
+(defvar *alias-to-marital-status-list*
   '(((#\n #\N) . "Never married")
     ((#\m #\M) . "Married")
     ((#\s #\S) . "Separated")
     ((#\d #\D) . "Divorced")
     ((#\w #\W) . "Widowed"))
-  "LIST of ALIST with a STRING specifying a marital status to a CHARACTER LIST.
-ALIST such (ALIAS LIST . MARITAL STATUS). ALIAS LIST is a CHARACTER LIST
-representing an alias of MARITAL STATUS. MARITAL STATUS is a STRING with the
-full name of a marital status.
+  "LIST containing ALIST of ALIAS LIST to MARITAL STATUS.
+ALIAS LIST is a CHARACTER LIST representing an alias of MARITAL STATUS.
+MARITAL STATUS is a STRING with the full name of a marital status.
 
 Example: '(((#\n #\N) . \"Never Married\") ((#\d #\D) \"Divorced\"))")
 
@@ -29,9 +55,8 @@ return its value."
 
 (defun marital-status (status)
   "Return the marital status corresponding to the passed character."
-  (or
-   (block search-status
-     (loop for item in *status-list* do
-       (when (member status (car item))
-         (return-from search-status (cdr item)))))
-   "Unknown character"))
+  (or (block search-status
+        (loop for item in *alias-to-marital-status-list* do
+          (when (member status (car item))
+            (return-from search-status (cdr item)))))
+      "Unknown character"))
