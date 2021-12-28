b49c1263cab06606e3dd720db1cd60e0ed7fa2db
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-12-28 23:30:39 +0100

    [refactor, add] Type checking and better doc for day-of-the-week

--- SelectionStructures/src/simpleAndDouble/day-of-the-week.lisp
+++ SelectionStructures/src/simpleAndDouble/day-of-the-week.lisp
@@ -2,7 +2,20 @@
 ;;;; shows its name
 ;;;; Example: 3 = "Wednesday"
 
-(defvar *days-of-the-week*
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun string-list-p (object)
+    "Return T if OBJECT is a LIST containing STRING.
+Otherwise, return NIL."
+    (when (listp object)
+      (every #'stringp object))))
+
+(deftype string-list ()
+  "Type for objects which must be a STRING LIST."
+  `(satisfies string-list-p))
+
+(declaim (type (string-list) *days-of-the-week*))
+
+(defconstant *days-of-the-week*
   '("Monday"
     "Tuesday"
     "Wednesday"
@@ -10,7 +23,8 @@
     "Friday"
     "Saturday"
     "Sunday")
-  "LIST of STRING specifying the name of a day of the week.")
+  "DAY-OF-THE-WEEK LIST.
+DAY-OF-THE-WEEK is a STRING specifying the name of a day of the week.")
 
 (defun main ()
   "Main function of the program."
@@ -22,10 +36,15 @@
 (declaim (ftype (function () integer) get-number))
 
 (defun get-number ()
-  "Ask the user to enter a number and return its value."
+  "Ask the user to enter INTEGER and return its value."
   (print "Enter a number: ")
   (read))
 
+(declaim (ftype (function (integer) string) name-of-nth-day))
+
 (defun name-of-nth-day (number)
-  "Return the name of the nth day of the week."
-  (nth (- number 1) *days-of-the-week*))
+  "Return DAY-OF-THE-WEEK from `*days-of-the-week*' corresponding to NUMBER.
+DAY-OF-THE-WEEK is a STRING.
+NUMBER is a INTEGER.
+Note: The first DAY-OF-THE-WEEK stands by 1, not 0."
+  (nth (1- number) *days-of-the-week*))
