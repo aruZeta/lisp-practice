e9ef5e0e6f150bea24b3d5976c9a4b259bd89727
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-12-29 22:55:16 +0100

    [add, refactor] Better docstrings for calculate-water-bill

--- SelectionStructures/src/simpleAndDouble/calculate-water-bill.lisp
+++ SelectionStructures/src/simpleAndDouble/calculate-water-bill.lisp
@@ -19,24 +19,33 @@ Otherwise, return NIL."
 (declaim (type (positive) *low-price*))
 
 (defvar *high-price* 10
-  "INT specifying the price of the liters above the `*high-limit*'.")
+  "PRICE of the LITERS above the `*high-limit*'.
+PRICE is a POSITIVE.")
 (defvar *medium-price* 4
-  "INT specifying the price of the liters between the `*medium-limit*' and
-`*high-limit*'.")
+  "PRICE of the LITERS between the `*medium-limit*' and `*high-limit*'.
+PRICE is a POSITIVE.")
 (defvar *low-price* 1
-  "INT specifying the price of the liters between the `*low-limit*' and
-`*medium-limit*'.")
+  "PRICE of the LITERS between the `*low-limit*' and `*medium-limit*'.
+PRICE is a POSITIVE.")
 
 (declaim (type (positive) *high-limit*))
 (declaim (type (positive) *medium-limit*))
 (declaim (type (positive) *low-limit*))
 
 (defvar *high-limit* 20
-  "INT specifying the high limit of liters of water consumption.")
+  "LIMIT specifying the high-limit of water consumption.
+Price of the LITERS consumed after this limit is determined by `*high-price*'.
+LIMIT is a POSITIVE.")
 (defvar *medium-limit* 10
-  "INT specifying the medium limit of liters of water consumption.")
+  "LIMIT specifying the medium-limit of water consumption.
+Price of the LITERS consumed between this limit and `*high-limit*' is determined
+by `*medium-price*'.
+LIMIT is a POSITIVE.")
 (defvar *low-limit* 0
-  "INT specifying the low limit of liters of water consumption.")
+  "LIMIT specifying the low-limit of water consumption.
+Price of the LITERS consumed between this limit and `*medium-limit*' is
+determined by `*low-price*'.
+LIMIT is a POSITIVE.")
 
 (defun main ()
   "Main function of the program."
@@ -48,15 +57,20 @@ Otherwise, return NIL."
 (declaim (ftype (function () positive) get-liters))
 
 (defun get-liters ()
-  "Ask the user to enter an amount of liters and return its value."
+  "Ask the user to enter LITERS and return its value.
+LITERS is a POSITIVE specifying an amount of liters."
   (print "Enter how much liters you've used this month: ")
   (read))
 
 (declaim (ftype (function (positive) positive) calc-water-bill))
 
 (defun calc-water-bill (liters)
-  "Return the total price of the liters of water used, depending if it above the
-high, medium or low limits."
+  "Return the total PRICE of LITERS.
+PRICE is a POSITIVE specifying the resulting price.
+LITERS is a POSITIVE specifying an amount of liters.
+
+Uses `calc-water-bill-high-limit', `calc-water-bill-medium-limit' and
+`calc-water-bill-low-limit' depending on the LIMIT exceeded by LITERS."
   (cond
     ((> liters *high-limit*) (+ (calc-water-bill-low-limit)
                                 (calc-water-bill-medium-limit)
@@ -69,7 +83,9 @@ high, medium or low limits."
                 calc-water-bill-high-limit))
 
 (defun calc-water-bill-high-limit (liters)
-  "Return the price of the liters minus `*high-limit*'."
+  "Return PRICE of LITERS minus `*high-limit*'.
+PRICE is a POSITIVE specifying the resulting price.
+LITERS is a POSITIVE specifying an amount of liters."
   (* (- liters *high-limit*)
      *high-price*))
 
@@ -77,9 +93,11 @@ high, medium or low limits."
                 calc-water-bill-medium-limit))
 
 (defun calc-water-bill-medium-limit (&optional liters)
-  "Return the price of the liters between `*high-limit*' and `*medium-limit*'.
-If an amount of liters is passed, return the price of the liters minus
-`*medium-limit*'.
+  "Return PRICE of `*high-limit*' minus `*medium-limit*'.
+Otherwise, if LITERS is passed, return PRICE of LITERS minus `*medium-limit*'.
+PRICE is a POSITIVE specifying the resulting price.
+LITERS is a POSITIVE specifying an amount of liters.
+
 Eg: (being all the variables as default)
 (calc-water-bill-medium-limit 15) => 20
 (calc-water-bill-medium-limit) => 40"
@@ -91,9 +109,11 @@ Eg: (being all the variables as default)
                 calc-water-bill-low-limit))
 
 (defun calc-water-bill-low-limit (&optional liters)
-  "Return the price of the liters between `*medium-limit*' and `*low-limit*'.
-If an amount of liters is passed, return the price of the liters minus
-`*low-limit*'.
+  "Return PRICE of `*medium-limit*' minus `*low-limit*'.
+Otherwise, if LITERS is passed, return PRICE of LITERS minus `*low-limit*'.
+PRICE is a POSITIVE specifying the resulting price.
+LITERS is a POSITIVE specifying an amount of liters.
+
 Eg: (being all the variables as default)
 (calc-water-bill-medium-limit 5) => 5
 (calc-water-bill-medium-limit) => 10"
