ac5eea84f9d6eedf706d531d51d87cdc94103bc8
aru-hackZ <aru_hackZ.official@zohomail.eu>
2021-12-29 22:54:43 +0100

    [add, refactor] Type checking for calculate-water-bill

--- SelectionStructures/src/simpleAndDouble/calculate-water-bill.lisp
+++ SelectionStructures/src/simpleAndDouble/calculate-water-bill.lisp
@@ -3,6 +3,21 @@
 ;;;; how much each liter of water costs depending on the limits
 ;;;; Example: 15l = 10l of low price + 5l of medium price
 
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun positivep (object)
+    "Return T if object is a positive NUMBER or 0.
+Otherwise, return NIL."
+    (when (numberp object)
+      (>= object 0))))
+
+(deftype positive ()
+  "Type for objects which must be a positive NUMBER or 0."
+  `(satisfies positivep))
+
+(declaim (type (positive) *high-price*))
+(declaim (type (positive) *medium-price*))
+(declaim (type (positive) *low-price*))
+
 (defvar *high-price* 10
   "INT specifying the price of the liters above the `*high-limit*'.")
 (defvar *medium-price* 4
@@ -11,6 +26,10 @@
 (defvar *low-price* 1
   "INT specifying the price of the liters between the `*low-limit*' and
 `*medium-limit*'.")
+
+(declaim (type (positive) *high-limit*))
+(declaim (type (positive) *medium-limit*))
+(declaim (type (positive) *low-limit*))
 
 (defvar *high-limit* 20
   "INT specifying the high limit of liters of water consumption.")
@@ -26,12 +45,14 @@
             "You'll have to pay $~a~&"
             (calc-water-bill liters))))
 
-(declaim (ftype (function () integer) get-liters))
+(declaim (ftype (function () positive) get-liters))
 
 (defun get-liters ()
   "Ask the user to enter an amount of liters and return its value."
   (print "Enter how much liters you've used this month: ")
   (read))
+
+(declaim (ftype (function (positive) positive) calc-water-bill))
 
 (defun calc-water-bill (liters)
   "Return the total price of the liters of water used, depending if it above the
@@ -44,10 +65,16 @@ high, medium or low limits."
                                   (calc-water-bill-medium-limit liters)))
     (t (calc-water-bill-low-limit liters))))
 
+(declaim (ftype (function (positive) positive)
+                calc-water-bill-high-limit))
+
 (defun calc-water-bill-high-limit (liters)
   "Return the price of the liters minus `*high-limit*'."
   (* (- liters *high-limit*)
      *high-price*))
+
+(declaim (ftype (function (&optional positive) positive)
+                calc-water-bill-medium-limit))
 
 (defun calc-water-bill-medium-limit (&optional liters)
   "Return the price of the liters between `*high-limit*' and `*medium-limit*'.
@@ -59,6 +86,9 @@ Eg: (being all the variables as default)
   (* (- (or liters *high-limit*)
         *medium-limit*)
      *medium-price*))
+
+(declaim (ftype (function (&optional positive) positive)
+                calc-water-bill-low-limit))
 
 (defun calc-water-bill-low-limit (&optional liters)
   "Return the price of the liters between `*medium-limit*' and `*low-limit*'.
