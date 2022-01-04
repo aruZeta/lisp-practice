76f0a0282e7fd7b906b82d90f3fa8a3bda395616
aru-hackZ <aru_hackZ.official@zohomail.eu>
2022-01-04 17:46:49 +0100

    [refactor, add] Type checking and better doc for ex6 - first-project

--- first-project/ex6.lisp
+++ first-project/ex6.lisp
@@ -3,20 +3,27 @@
 
 (uiop:define-package :lisp-practice/first-project/ex6
   (:use :cl)
+  (:import-from :lisp-practice/utils/types #:int-list)
   (:import-from :lisp-practice/utils/get-input #:get-money)
   (:export main))
 (in-package :lisp-practice/first-project/ex6)
 
+(declaim (type (int-list) *bills*))
+
 (defvar *bills* '(500 200 100 50 20 10 5)
-  "LIST of INT specifying the value of the available bills.")
+  "INTEGER LIST specifying the value of the available bills.")
 
 (defun main ()
   "Main function of the program."
   (let ((money (get-money)))
     (print-bills money)))
 
+(declaim (ftype (function (integer)) print-bills))
+
 (defun print-bills (money)
-  "Shows the money in bills, the bills showed are stored in `*bills*'."
+  "Shows how much of each BILL is necessary to reach MONEY.
+MONEY is a INTEGER.
+BILL is a INTEGER specifying a bill in `*bills*'."
   (loop for bill in *bills* do
     (format t
             "Bills of ~a: ~a~&"
